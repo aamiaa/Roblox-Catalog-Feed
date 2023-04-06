f739e31827729d6e9875f36a82c643f754c70b25
aamiaa <9750071+aamiaa@users.noreply.github.com>
2023-04-06 15:45:06 +0200

    forgor to exclude build folder

--- build/util/sleep.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sleep(ms) {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve, ms);
-    });
-}
-exports.default = sleep;
